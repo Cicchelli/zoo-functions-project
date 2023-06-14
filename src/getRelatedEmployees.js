3c251bc5a61b57dbbb1643d0239f44c386652205
Stephan <cicchelli.stephan@gmail.com>
2023-06-14 16:12:05 -0300

    req getRelatedEmployees

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,15 @@
-const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 
-const isManager = (id) => {
-  // seu código aqui
-};
+const isManager = (id) => employees.some((employee) =>
+  employee.managers.find((manager) => manager === id));
 
 const getRelatedEmployees = (managerId) => {
-  // seu código aqui
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  return employees
+    .filter((employee) => employee.managers.includes(managerId))
+    .map((person) => `${person.firstName} ${person.lastName}`);
 };
 
 module.exports = { isManager, getRelatedEmployees };
