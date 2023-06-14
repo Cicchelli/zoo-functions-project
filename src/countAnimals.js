1c7bdeb1d601dca5122eb3e2d186ad1299cc9689
Stephan <cicchelli.stephan@gmail.com>
2023-06-14 17:07:32 -0300

    req countAnimals

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,22 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 const countAnimals = (animal) => {
-  // seu código aqui
+  if (animal) {
+    const selectedSpecie = species.filter((specie) => specie.name === animal.species);
+    const residentsOfSex = selectedSpecie[0]
+      .residents.filter((resident) => {
+        if (animal.sex) {
+          return resident.sex === animal.sex;
+        }
+        return true;
+      });
+    return residentsOfSex.length;
+  }
+  return species.reduce((obj, specie) => {
+    const object = obj;
+    object[specie.name] = specie.residents.length;
+    return obj;
+  }, {});
 };
 
 module.exports = countAnimals;
