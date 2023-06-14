de035be189bf654937a1b754576c223a6aae58cc
Stephan <cicchelli.stephan@gmail.com>
2023-06-14 15:51:44 -0300

    req getEmployeeByName

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,13 @@
-const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 
 const getEmployeeByName = (employeeName) => {
-  // seu código aqui
+  // condição de verificação para quando não houver parametros ao chamar a função retorne um objeto vazio.
+  if (employeeName === undefined) return {};
+  // usei a HOF find para encontrar o elemento de acordo com firstName e lastName do colaborador, e o retorno será o objeto completo que atenda aos parametros informados na chamada da função.
+  return employees.find(
+    (employee) =>
+      employee.firstName === employeeName || employee.lastName === employeeName,
+  );
 };
 
 module.exports = getEmployeeByName;
