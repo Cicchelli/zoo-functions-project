fd9c5cdc9b43ea2246df787a2918ec0563b19841
Stephan <cicchelli.stephan@gmail.com>
2023-06-14 15:30:50 -0300

    req getSpeciesByIds

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,40 +1,7 @@
 const data = require('../data/zoo_data');
 
-const countAnimals = (animals) => {
-  
+const countAnimals = (animal) => {
+  // seu código aqui
 };
-// const objetoQtdAnimais = data.species.reduce((acc, specie) => {
-//   acc[specie.name] = data.specie.residents.length;
-//   return acc;
-// }, 0);
-// return objetoQtdAnimais;
-console.log(countAnimals());
-
-// const qtdAnimaisResidentes = (animal) =>
-//   data.species.reduce(
-//     (sum, spec) => (spec.name === animal.specie ? spec.residents : sum),
-//     0,
-//   ).length;
-
-// const qtdAnimaisSexo = (animal) => {
-//   const filtroAnimais = data.species.find(
-//     (specie) => specie.name === animal.specie,
-//   ).residents;
-//   const animals = filtroAnimais.reduce(
-//     (acc, sexo) => (sexo.sex === animal.sex ? acc + 1 : acc),
-//     0,
-//   );
-//   return animals;
-// };
-
-// function countAnimals(animal) {
-//   if (animal === undefined) {
-//     return objetoQtdAnimais;
-//   }
-//   if (Object.values(animal).length === 1) {
-//     return qtdAnimaisResidentes(animal);
-//   }
-//   return qtdAnimaisSexo(animal);
-// }
 
 module.exports = countAnimals;
