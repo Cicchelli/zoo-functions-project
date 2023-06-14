af4bf236e28a12d5659f64a83eca5c282d01fc47
Stephan <cicchelli.stephan@gmail.com>
2023-06-14 15:28:32 -0300

    req getSpeciesByIds

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,5 +1,7 @@
 const data = require('../data/zoo_data');
 
-const getSpeciesByIds = (...ids) => data.species.filter((animal) => ids.some((id) => animal.id === id));
+const getSpeciesByIds = (...ids) => {
+  data.species.filter((animal) => ids.some((id) => animal.id === id));
+};
 
 module.exports = getSpeciesByIds;
