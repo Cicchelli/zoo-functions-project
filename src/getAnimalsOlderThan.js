904054652b1e0a202a659e4b6e7228a56ff1c0bb
Stephan <cicchelli.stephan@gmail.com>
2023-06-14 15:38:36 -0300

    req getAnimalsOlderThan

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
 const getAnimalsOlderThan = (animal, age) => {
-  // seu código aqui
+  const species = data.species.find((specie) => specie.name === animal);
+  let hasMinimumAge = true;
+  species.residents.forEach((resident) => {
+    if (resident.age < age) hasMinimumAge = false;
+  });
+  return hasMinimumAge;
 };
 
 module.exports = getAnimalsOlderThan;
